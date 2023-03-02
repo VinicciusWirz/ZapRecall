2d2fddc7626cb4bda4fd55982edfa1f51ca44b12
Viniccius Wirz <vinimovel23@gmail.com>
2023-03-01 23:28:43 -0300

    Apply welcome page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Reset from "./GlobalStyles/Reset";
 import GlobalStyle from "./GlobalStyles/GlobalStyle";
 import MainPage from "./components/MainPage/MainPage";
+import StartingPage from "./components/StartingPage/StartingPage";
 
 function App() {
   return (
     <>
       <Reset />
       <GlobalStyle />
+      <StartingPage />
       <MainPage />
     </>
   );
---
+++ src/components/StartingPage/StartingPage.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "../../assets/logo.png"
+import { FrontPage } from "./styled";
+
+
+export default function StartingPage() {
+    const [display, setDisplay] = useState('flex');
+    return (
+        <FrontPage display={display}>
+            <img src={logo} />
+            <h1>ZapRecall</h1>
+            <button onClick={() => setDisplay('none')} data-test="start-btn">Iniciar Recall!</button>
+        </FrontPage>
+    );
+}
